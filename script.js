2f3a3ee287a7135c162ec9804523f03816530c82
Psyh2409 <psyh2409@gmail.com>
2021-11-24 10:44:52 +0200

    add funcs and objs

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+// classes
 class Robot {
     constructor() {
         this.work = () => {
             console.log("I'm Robot - I just work.")
         }
-        this.run = () => {this.work()}
+        this.run = () => {this.work()}// for inheriting demonstration
     }
 }
 let rbt = new Robot();
@@ -25,5 +26,45 @@ class RobotCooker extends Robot {
     work = () => {console.log("I'm RobotCooker - I just cook.")}
 }
 let ckr = new RobotCooker();
-let arr = [rbt, cff, dncr, ckr]
-
+let robots = [rbt, cff, dncr, ckr]
+// functions
+let droids = [];
+function Droid(txt) {
+    this.txt = txt;
+    this.work = () => {console.log(this.txt)}
+}
+droids.push(new Droid("I'm Droid - I just work."));
+function CoffeeDroid(x) {
+    Droid.call(this);
+    this.txt = x;
+}
+droids.push(new CoffeeDroid("I'm CoffeeDroid - I boil coffee."));
+function DroidDancer() {
+    Droid.call(this);
+    this.txt = "I'm DroidDancer - I just dance.";
+}
+droids.push(new DroidDancer());
+function DroidCooker() {
+    Droid.call(this);
+    this.txt = "I'm DroidCooker - I just cook.";
+}
+droids.push(new DroidCooker());
+// objects
+let bots = [];
+let bot = {
+    txt: "I'm Bot - I just work.",
+    work: (x)=>{console.log(x)}
+}
+let cfb = {
+    txt: "I'm CoffeeBot - I boil coffee."
+}
+cfb.__proto__ = bot;
+let dnb = {
+    txt: "I'm BotDancer - I just dance."
+}
+dnb.__proto__ = bot;
+let ckb = {
+    txt: "I'm BotCooker - I just cook."
+}
+ckb.__proto__ = bot;
+bots = [bot, cfb, dnb, ckb];
